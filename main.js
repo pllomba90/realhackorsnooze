675e64c548bde422d7d1e72ac6f7f51e3a51fa30
Peter Lombardo <peterllombardo@gmail.com>
2023-03-21 05:11:22 -0400

    final changes

--- main.js
+++ main.js
@@ -13,7 +13,11 @@ const $signupForm = $("#signup-form");
 const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
-
+const $navSubmitStory = $("#nav-submit-story");
+const $ownStories = $("#nav-my-stories");
+const $submitForm = $("#submit-form");
+const $storiesLists = $(".stories-list");
+const $userProfile = $("#user-profile");
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
  * calling this, individual components can re-show just what they want.
